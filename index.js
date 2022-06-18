274989af2a279cc3015e1312d289fb504c0a714d
moses ngige <moses32ngigz@gmail.com>
2022-06-18 11:43:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord= word.split("").reverse().join("");
+  return word === reversedWord ? true :false;
 }
+console.log(isPalindrome('madam'))
 
 /* 
   Add your pseudocode here
-*/
+//my function tekes the word 
+//turn the word into an array,reverses the array and converts it back to a string (z)
+//the new string is stored in a new variable reverseWord
+//finally using a ternary operator compares word to reverseWord to give true or false 
 
 /*
   Add written explanation of your solution here
+my function tekes the word 
+turn the word into an array,reverses the array and converts it back to a string (z)
+the new string is stored in a new variable reverseWord
+finally using a ternary operator compares word to reverseWord to give true or false 
+
 */
 
 // You can run `node index.js` to view these console logs
